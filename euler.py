1f815bf31900f6f42dd3c7e5ec125c6e2cf20deb
Thato Thapo <thatothetman@gmail.com>
2025-03-31 13:43:34 +0200

    Added Euler's method file

---
+++ euler.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  9 10:33:29 2019
+
+@author: Trancrypt
+"""
+
+import numpy as np
+
+a=0.0
+b=2.0
+N=10
+alp=0.5
+
+h=(b-a)/N
+t=a
+w=alp
+
+def f(t,y):
+    return y-(t**2)+1
+
+for i in range(N+1):
+    yt=((t+1)**2)-0.5*np.exp(t)
+    print(w, " ", yt)
+    w=w+h*f(t,w)
+    t=h*(i+1)
